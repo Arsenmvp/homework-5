3a2f57539419b5464f8310cbc8c4c8ca931f1b70
Arsenmvp <arsenklaptentko@gmail.com>
2025-06-19 09:15:42 +0300

    Update task2.js

--- task2.js
+++ task2.js
@@ -1,1 +1,17 @@
+function format(data, type) {
+  if (type === "number") {
+    format = function(data) {
+      return Number(data);
+    };
+  } else if (type === "string") {
+    format = function(data) {
+      return String(data);
+    };
+  } else if (type === "boolean") {
+    format = function(data) {
+      return Boolean(data);
+    };
+  }
+  return format(data);
+}
 
